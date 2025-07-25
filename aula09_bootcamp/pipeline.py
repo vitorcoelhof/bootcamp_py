a68c308e6104a638a0b7c73267a0e56bac1b8219
vitorcoelhof <vitorcoelhof@gmail.com>
2025-07-25 08:38:11 -0300

    adicionando aula09

---
+++ aula09_bootcamp/pipeline.py
@@ -0,0 +1,6 @@
+from aula08_bootcamp.etl import pipeline_calcular_kpi_de_vendas_consolidado
+
+pasta_argumento: str = 'data'
+formato_de_saida: list = ["csv", "parquet"]
+
+pipeline_calcular_kpi_de_vendas_consolidado(pasta_argumento, formato_de_saida)
